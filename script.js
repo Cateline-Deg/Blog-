aac1e74a8d9cc987d6b22b7c2c1dfdd629768e4c
Cateline1108-14 <7cg9wtzypk@privaterelay.appleid.com>
2026-08-07 20:06:20 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+const boutonTheme = document.querySelector("#bouton-theme");
+const formulaire = document.querySelector("#formulaire-contact");
+const champNom = document.querySelector("#nom");
+const annee = document.querySelector("#annee");
+
+// Année automatique
+annee.textContent = new Date().getFullYear();
+
+// Mode sombre
+boutonTheme.addEventListener("click", function () {
+    document.body.classList.toggle("mode-sombre");
+
+    if (document.body.classList.contains("mode-sombre")) {
+        boutonTheme.textContent = "Désactiver le mode sombre";
+    } else {
+        boutonTheme.textContent = "Activer le mode sombre";
+    }
+});
+
+// Formulaire
+formulaire.addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    alert("Merci " + champNom.value + " ! Ton message a bien été préparé.");
+
+    formulaire.reset();
+});
+
+const recherche = document.querySelector("#recherche-articles");
+const articles = document.querySelectorAll(".grille-articles article");
+
+recherche.addEventListener("input", function () {
+    const texteRecherche = recherche.value.toLowerCase();
+
+    articles.forEach(function (article) {
+        const titre = article.querySelector("h3").textContent.toLowerCase();
+
+        if (titre.includes(texteRecherche)) {
+            article.style.display = "flex";
+        } else {
+            article.style.display = "none";
+        }
+    });
+});
